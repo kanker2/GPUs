857a5df44a34deda00081cc81a0f121e2912da9f
karce <karce@ucm.es>
2024-03-14 14:04:13 +0100

    Sin mas he hecho copia y pega

--- src/lab1/lane-assist/routinesGPU.cu
+++ src/lab1/lane-assist/routinesGPU.cu
@@ -5,103 +5,14 @@
 
 #include "routinesGPU.h"
 
+#define BLOCK_SIZE 32
+
 void canny(uint8_t *im, uint8_t *image_out,
 	float *NR, float *G, float *phi, float *Gx, float *Gy, uint8_t *pedge,
 	float level,
 	int height, int width)
 {
-	int i, j;
-	int ii, jj;
-	float PI = 3.141593;
-
-	float lowthres, hithres;
-
-	for(i=2; i<height-2; i++)
-		for(j=2; j<width-2; j++)
-		{
-			// Noise reduction
-			NR[i*width+j] =
-				 (2.0*im[(i-2)*width+(j-2)] +  4.0*im[(i-2)*width+(j-1)] +  5.0*im[(i-2)*width+(j)] +  4.0*im[(i-2)*width+(j+1)] + 2.0*im[(i-2)*width+(j+2)]
-				+ 4.0*im[(i-1)*width+(j-2)] +  9.0*im[(i-1)*width+(j-1)] + 12.0*im[(i-1)*width+(j)] +  9.0*im[(i-1)*width+(j+1)] + 4.0*im[(i-1)*width+(j+2)]
-				+ 5.0*im[(i  )*width+(j-2)] + 12.0*im[(i  )*width+(j-1)] + 15.0*im[(i  )*width+(j)] + 12.0*im[(i  )*width+(j+1)] + 5.0*im[(i  )*width+(j+2)]
-				+ 4.0*im[(i+1)*width+(j-2)] +  9.0*im[(i+1)*width+(j-1)] + 12.0*im[(i+1)*width+(j)] +  9.0*im[(i+1)*width+(j+1)] + 4.0*im[(i+1)*width+(j+2)]
-				+ 2.0*im[(i+2)*width+(j-2)] +  4.0*im[(i+2)*width+(j-1)] +  5.0*im[(i+2)*width+(j)] +  4.0*im[(i+2)*width+(j+1)] + 2.0*im[(i+2)*width+(j+2)])
-				/159.0;
-		}
-
-
-	for(i=2; i<height-2; i++)
-		for(j=2; j<width-2; j++)
-		{
-			// Intensity gradient of the image
-			Gx[i*width+j] = 
-				 (1.0*NR[(i-2)*width+(j-2)] +  2.0*NR[(i-2)*width+(j-1)] +  (-2.0)*NR[(i-2)*width+(j+1)] + (-1.0)*NR[(i-2)*width+(j+2)]
-				+ 4.0*NR[(i-1)*width+(j-2)] +  8.0*NR[(i-1)*width+(j-1)] +  (-8.0)*NR[(i-1)*width+(j+1)] + (-4.0)*NR[(i-1)*width+(j+2)]
-				+ 6.0*NR[(i  )*width+(j-2)] + 12.0*NR[(i  )*width+(j-1)] + (-12.0)*NR[(i  )*width+(j+1)] + (-6.0)*NR[(i  )*width+(j+2)]
-				+ 4.0*NR[(i+1)*width+(j-2)] +  8.0*NR[(i+1)*width+(j-1)] +  (-8.0)*NR[(i+1)*width+(j+1)] + (-4.0)*NR[(i+1)*width+(j+2)]
-				+ 1.0*NR[(i+2)*width+(j-2)] +  2.0*NR[(i+2)*width+(j-1)] +  (-2.0)*NR[(i+2)*width+(j+1)] + (-1.0)*NR[(i+2)*width+(j+2)]);
-
-
-			Gy[i*width+j] = 
-				 ((-1.0)*NR[(i-2)*width+(j-2)] + (-4.0)*NR[(i-2)*width+(j-1)] +  (-6.0)*NR[(i-2)*width+(j)] + (-4.0)*NR[(i-2)*width+(j+1)] + (-1.0)*NR[(i-2)*width+(j+2)]
-				+ (-2.0)*NR[(i-1)*width+(j-2)] + (-8.0)*NR[(i-1)*width+(j-1)] + (-12.0)*NR[(i-1)*width+(j)] + (-8.0)*NR[(i-1)*width+(j+1)] + (-2.0)*NR[(i-1)*width+(j+2)]
-				+    2.0*NR[(i+1)*width+(j-2)] +    8.0*NR[(i+1)*width+(j-1)] +    12.0*NR[(i+1)*width+(j)] +    8.0*NR[(i+1)*width+(j+1)] +    2.0*NR[(i+1)*width+(j+2)]
-				+    1.0*NR[(i+2)*width+(j-2)] +    4.0*NR[(i+2)*width+(j-1)] +     6.0*NR[(i+2)*width+(j)] +    4.0*NR[(i+2)*width+(j+1)] +    1.0*NR[(i+2)*width+(j+2)]);
-
-			G[i*width+j]   = sqrtf((Gx[i*width+j]*Gx[i*width+j])+(Gy[i*width+j]*Gy[i*width+j]));	//G = √Gx²+Gy²
-			phi[i*width+j] = atan2f(fabs(Gy[i*width+j]),fabs(Gx[i*width+j]));
-
-			if(fabs(phi[i*width+j])<=PI/8 )
-				phi[i*width+j] = 0;
-			else if (fabs(phi[i*width+j])<= 3*(PI/8))
-				phi[i*width+j] = 45;
-			else if (fabs(phi[i*width+j]) <= 5*(PI/8))
-				phi[i*width+j] = 90;
-			else if (fabs(phi[i*width+j]) <= 7*(PI/8))
-				phi[i*width+j] = 135;
-			else phi[i*width+j] = 0;
-		}
-
-	// Edge
-	for(i=3; i<height-3; i++)
-		for(j=3; j<width-3; j++)
-		{
-			pedge[i*width+j] = 0;
-			if(phi[i*width+j] == 0){
-				if(G[i*width+j]>G[i*width+j+1] && G[i*width+j]>G[i*width+j-1]) //edge is in N-S
-					pedge[i*width+j] = 1;
-
-			} else if(phi[i*width+j] == 45) {
-				if(G[i*width+j]>G[(i+1)*width+j+1] && G[i*width+j]>G[(i-1)*width+j-1]) // edge is in NW-SE
-					pedge[i*width+j] = 1;
-
-			} else if(phi[i*width+j] == 90) {
-				if(G[i*width+j]>G[(i+1)*width+j] && G[i*width+j]>G[(i-1)*width+j]) //edge is in E-W
-					pedge[i*width+j] = 1;
-
-			} else if(phi[i*width+j] == 135) {
-				if(G[i*width+j]>G[(i+1)*width+j-1] && G[i*width+j]>G[(i-1)*width+j+1]) // edge is in NE-SW
-					pedge[i*width+j] = 1;
-			}
-		}
-
-	// Hysteresis Thresholding
-	lowthres = level/2;
-	hithres  = 2*(level);
-
-	for(i=3; i<height-3; i++)
-		for(j=3; j<width-3; j++)
-		{
-			image_out[i*width+j] = 0;
-			if(G[i*width+j]>hithres && pedge[i*width+j])
-				image_out[i*width+j] = 255;
-			else if(pedge[i*width+j] && G[i*width+j]>=lowthres && G[i*width+j]<hithres)
-				// check neighbours 3x3
-				for (ii=-1;ii<=1; ii++)
-					for (jj=-1;jj<=1; jj++)
-						if (G[(i+ii)*width+j+jj]>hithres)
-							image_out[i*width+j] = 255;
-		}
+	
 }
 
 
